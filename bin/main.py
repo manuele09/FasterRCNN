c714f8fdae1e9264394ec2c757ec86f51d0abe25
Emanuele <emacannizzo@gmail.com>
2023-06-13 22:22:55 +0200

    Added bin dir; modified base_path

--- bin/main.py
+++ bin/main.py
@@ -31,24 +31,18 @@ RESIZE_TO = 1000 # resize the image for training and transforms
 NUM_EPOCHS = 1 # number of epochs to train for
 NUM_WORKERS = 4
 
-
-subprocess.run(["rm", "-rf", "logs"])
-#tensorboard_process = subprocess.Popen(['tensorboard', '--logdir', 'logs']) 
+base_path = ".."
+subprocess.run(["rm", "-rf", base_path + "/logs"])
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Resize(( RESIZE_TO, RESIZE_TO), antialias=True)
                                , transforms.Lambda(remove_alpha_channel)])
 
-# transform = transforms.Compose([transforms.ToTensor()
-#                                , transforms.Lambda(remove_alpha_channel)])
-
-dataset = RealDataset("real_dataset/train", "real_dataset/train/list_v2.txt", transform=transform)
-
-
+dataset = RealDataset(base_path + "/real_dataset/train", base_path + "/real_dataset/train/list_v2.txt", transform=transform)
 
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, collate_fn=collate_fn)
 
 #dataset.show_bounding(54)
-full_model = FasterModel("models_saved", last_epoch=-1, last_batch=-1)
+full_model = FasterModel(base_path, last_epoch=-1, last_batch=-1)
 
 
 
